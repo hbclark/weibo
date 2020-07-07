1be96f992e7a8be8e1fb0833514c51f1ceb4a7b4
clark <arcaneade@gmail.com>
2020-07-07 09:48:53 +0000

    static pages

---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','Home Page')
+@section('content')
+  <h1>Home Page</h1>
+@stop
--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help','StaticPagesController@help');
+Route::get('./about','StaticPagesController@about');
